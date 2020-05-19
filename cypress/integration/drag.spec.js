db1d13c3ad487ffaf20caefb29584c9b2e4e01f5
Lewis Prescott <Lewis.Prescott@cancer.org.uk>
2020-05-19 14:25:07 +0100

    with wrap

--- cypress/integration/drag.spec.js
+++ cypress/integration/drag.spec.js
@@ -9,14 +9,16 @@ describe('Drag', () => {
         cy.get('[data-rbd-drag-handle-draggable-id]')
         .eq(1).as('first').should('contain', 'id:2');
         cy.get('[data-rbd-drag-handle-draggable-id]').eq(2).should('contain', 'id:3');
-
-        cy.get('@first')
-        .focus()
-        .trigger('keydown', { keyCode: 32})
-        .get('@first')
-        .trigger('keydown', { keyCode: 40, force: true })
-        .wait(200)
-        .trigger('keydown', {keyCode: 32, force: true});
+        
+        cy.get('@first').within(_$ => {
+            cy.wrap(_$)
+            .focus()
+            .trigger('keydown', { keyCode: 32})
+            .get('@first')
+            .trigger('keydown', { keyCode: 40, force: true })
+            .wait(200)
+            .trigger('keydown', {keyCode: 32, force: true});
+        })
 
         cy.get('[data-rbd-drag-handle-draggable-id]').eq(1).should('contain', 'id:3');
 
